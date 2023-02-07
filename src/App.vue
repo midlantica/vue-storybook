1ce3d8c21f00433f97fcf0a4b888bd5452bf306e
midlantica <druharper@gmail.com>
2023-02-06 18:07:14 -0600

    save

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <header class="leading-normal flex self-start flex-wrap">
+    <img alt="Vue logo" class="block" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,24 +18,11 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
@@ -43,5 +30,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
--- src/stories/Header.vue
+++ src/stories/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <header>
     <div class="flex justify-between items-center border-b border-black/[.1] px-[15px] py-[20px] font-sans">
-      <div>
-        <svg class="inline" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <div class="flex items-center gap-x-2">
+        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <g fill="none" fill-rule="evenodd">
             <path d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z" fill="#FFF" />
             <path d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z" fill="#555AB9" />
             <path d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z" fill="#91BAF8" />
           </g>
         </svg>
-        <h1 class="text-gray-500">Acme</h1>
+        <h1 class="text-gray-500 font-black leading-none text-[20px] inline">Acme</h1>
       </div>
       <div class="flex gap-4 items-center">
         <span class="text-[#333] text-sm" v-if="user">Welcome, <b>{{ user.name }}</b>!</span>
@@ -22,14 +22,11 @@
 </template>
 
 <script>
-  import './header.css'
   import MyButton from './Button.vue'
 
   export default {
     name: 'my-header',
-
     components: { MyButton },
-
     props: {
       user: {
         type: Object,
